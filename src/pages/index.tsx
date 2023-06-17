a15e0db509dc8265547ef624fe314e4f3bc1da89
Yamsh39 <s1f102300043@iniad.org>
2023-06-17 16:46:17 +0900

    9*9で表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,53 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+  //0はなし,1はフラグ,2は？,3は穴あけ
+  const [bombMap, setBombMap] = useState<number[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  //1がボム,0が安置
+
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  );
+  const board: number[][] = [];
+  let zeroList: { x: number; y: number }[];
+
+  const onClick = (x: number, y: number) => {};
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <div className={styles.hoge} />
-      </main>
+      <div className={styles.map}>
+        {userInputs.map((row, y) =>
+          row.map((num, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {isPlaying && <div className={styles.sign} />}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
