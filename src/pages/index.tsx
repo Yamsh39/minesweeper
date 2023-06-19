778e04079f9c238f13c982f2c21a9fb41d442b00
Yamsh39 <s1f102300043@iniad.org>
2023-06-20 04:24:58 +0900

    右クリック判定を除く基本動作完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './index.module.css';
+
 const normalBoard: 0[][] = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,7 +20,7 @@ const Home = () => {
   const newBombMap: (0 | 1)[][] = JSON.parse(JSON.stringify(bombMap));
   //1がボム,0が安置
   const board: number[][] = [
-    [-1, -1, -1 - 1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -31,43 +32,10 @@ const Home = () => {
   ];
   //-1はあけられてない状態、0が空白,1~8が数,9は?、10は旗
   //-1:未開封, 0:空, 1~8:隣接するボムの数, 9:疑問マーク, 10:旗マーク, 11:ボム
-  //いったん保存
-  // [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  // [10, 11, 12, 13, 14, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  // [0, 0, 0, 0, 0, 0, 0, 0, 0],
-
   const isPlaying = bombMap.some((row) => row.includes(1));
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
-  // let zeroList: { x: number; y: number }[];
-  // let playCount = 0;
-
-  const boardUpdate = () => {
-    bombMap.forEach((row, y) => {
-      row.forEach((value, x) => {
-        if (value === 1) {
-          board[y][x] = 11;
-        }
-      });
-    });
-    userInputs.forEach((row, y) => {
-      row.forEach((value, x) => {
-        if (value === 2) {
-          board[y][x] = 9;
-        } else if (value === 3) {
-          board[y][x] = 10;
-        }
-      });
-    });
-  };
-
   const directions = [
     [-1, 0],
     [-1, -1],
@@ -78,6 +46,25 @@ const Home = () => {
     [0, 1],
     [-1, 1],
   ];
+  //都度boardを更新
+  // const boardUpdate = () => {
+  //   bombMap.map((row, y) => {
+  //     row.map((value, x) => {
+  //       if (value === 1) {
+  //         board[y][x] = 11;
+  //       }
+  //     });
+  //   });
+  //   userInputs.map((row, y) => {
+  //     row.map((value, x) => {
+  //       if (value === 2) {
+  //         board[y][x] = 9;
+  //       } else if (value === 3) {
+  //         board[y][x] = 10;
+  //       }
+  //     });
+  //   });
+  // };
 
   //周りのボムの数の探索
   const check8 = (x: number, y: number) => {
@@ -86,11 +73,17 @@ const Home = () => {
       for (const direction of directions) {
         const newX = x + direction[0];
         const newY = y + direction[1];
-        if (bombMap[newY] !== undefined && bombMap[newY][newX] && bombMap[newY][newX] === 1) {
+        if (
+          bombMap[newY] !== undefined &&
+          bombMap[newY][newX] !== undefined &&
+          bombMap[newY][newX] === 1
+        ) {
           bombCount += 1;
         }
       }
-      board[y][x] = bombCount;
+      if (bombMap[y][x] !== 1) {
+        board[y][x] = bombCount;
+      }
       if (bombCount === 0) {
         for (const direction of directions) {
           const newX = x + direction[0];
@@ -100,7 +93,6 @@ const Home = () => {
       }
     }
   };
-
   //bombMapの走査
   const checkBoard = () => {
     for (let x = 0; x < bombMap.length; x++) {
@@ -123,7 +115,7 @@ const Home = () => {
       newBombMap[newY][newX] = 1;
     }
   };
-  //computed→計算値
+
   const onClick = (x: number, y: number) => {
     newUserInputs[y][x] = 1;
     setUserInputs(newUserInputs);
@@ -137,21 +129,28 @@ const Home = () => {
       setBombMap(newBombMap);
       //ボムは正常に配置可能
       checkBoard();
+      // boardUpdate();
     }
     console.log('x,y', x, y);
     //ゲームが終了しているかの確認
-    if (bombMap[y] !== undefined && bombMap[y][x] !== undefined && isFailure) {
+    if (isFailure) {
       console.log('game over');
+      bombMap.map((row, y) => {
+        row.map((value, x) => {
+          if (value === 1) {
+            board[y][x] = 11;
+          }
+        });
+      });
     }
-    checkBoard();
-    boardUpdate();
-    console.table(newBombMap);
-    console.table(board);
-    console.log(isPlaying);
-    console.log(newBombMap.flat().filter(Boolean).length);
   };
 
-  //returnないでboardを参照しているのにboardでuseStateを使わないとなるとどのように値を保持すればよいのか
+  checkBoard();
+  // boardUpdate();
+  console.table(newBombMap);
+  console.table(board);
+  console.log(isPlaying);
+  console.log(newBombMap.flat().filter(Boolean).length);
   return (
     <div className={styles.container}>
       <div className={styles.map}>
@@ -160,23 +159,24 @@ const Home = () => {
             <div
               className={styles.cell}
               key={`${x}-${y}`}
-              onClick={() => onClick(x, y)}
+              onClick={isFailure ? undefined : () => onClick(x, y)}
               style={
                 isPlaying
                   ? {
                       boxShadow:
-                        num === -1 || num === 11
+                        (!isFailure && num === 11) || num === -1
                           ? '4px 4px 3px #fff inset, -4px -4px 3px #808080 inset'
                           : '0 0',
                     }
                   : { boxShadow: '4px 4px 3px #fff inset, -4px -4px 3px #808080 inset' }
               }
             >
+              {/* {board[y][x]} */}
               {isPlaying && (
                 <div
                   className={styles.sign}
                   style={{
-                    backgroundPosition: 30 * (1 - num),
+                    backgroundPositionX: 30 * (1 - num),
                   }}
                 />
               )}
